f693413dc598ce18d4da461b324a57aad6ec9027
Victoria Douka-Doukopoulou <doukavictoria@gmail.com>
2019-04-24 00:02:10 +0200

    new fancy stuffss

--- script.js
+++ script.js
@@ -25,3 +25,52 @@ var sites = [
         $("#third_link").show();
       console.log('works?')
   };
+
+  function bio() {
+     $("#first_bio").hide();
+     $("#first_bio").show();
+      $("#second_bio").show();
+      console.log('works?')
+  };
+  function bye() {
+     $("#first_bio").hide();
+     $("#first_bio").show();
+      $("#second_bio").hide();
+      console.log('works?')
+  };
+  function book() {
+     $("#book_state").hide();
+     $("#second_book").show();
+     $("#third_book").show();
+     $("#purchase").show();
+      console.log('works?')
+  };
+  function bye_book() {
+     $("#book_state").show();
+     $("#second_book").hide();
+     $("#third_book").hide();
+     $("#purchase").hide();
+      console.log('works?')
+  };
+
+// MOBILE
+
+function menu() {
+   // $("#menu_bio").toggle();
+    $("#menu_bio").toggleClass('showbio');
+    console.log('works?')
+}
+
+function menu_correspondance() {
+      $("#menu_first_link").hide();
+    $("#menu_second_link").show();
+      $("#menu_third_link").show();
+    console.log('works?')
+};
+function menu_book() {
+   $("#menu_book_state").hide();
+   $("#menu_second_book").toggle();
+   $("#menu_third_book").toggle();
+   $("#menu_purchase").toggle();
+    console.log('works?')
+};
